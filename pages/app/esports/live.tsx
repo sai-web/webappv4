d56880d15af5136609950f0f94a17bf8fbbb074c
saisumith770 <saisumith812@gmail.com>
2020-11-20 10:55:11 +0400

    restructured the filing system

---
+++ pages/app/esports/live.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+import { Games } from '../../../components/home/esports/games'
+import { PageModes } from '../../../components/nav/top/components/pageMode'
+import { Overlay } from '../../../components/home/esports/overlay'
+import { StreamSet } from '../../../components/home/esports/StreamSet'
+import Template from '../../../components/app/template'
+
+const Pages: React.FC = () => {
+    return (
+        <>
+            <PageModes name="Live" page="Live" logo={<i className="fa fa-bullseye" style={{ fontSize: "13px" }}></i>} />
+            <PageModes name="Clips" page="Live" logo={<span className="material-icons" style={{ fontSize: "15px" }}>insert_link</span>} />
+        </>
+    )
+}
+
+function Esports() {
+    return (
+        <Template PageMode={<Pages />} width="150px" page="Esports" >
+            <div style={{
+                width: "100%",
+                height: "200px",
+                overflow: "hidden"
+            }}
+                className="main-content-div"
+            >
+                <Overlay />
+            </div>
+            <Games />
+            <StreamSet />
+        </Template>
+    )
+}
+
+export default Esports
---
+++ pages/app/music/tracks.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { music } from '../../../components/home/fakeData/music'
+
+import { PageModes } from '../../../components/nav/top/components/pageMode'
+import { Overlay } from '../../../components/home/music/overlay'
+import { AllTracks } from '../../../components/home/music/tracks'
+import Template from '../../../components/app/template'
+
+const Pages: React.FC = () => {
+    return (
+        <>
+            <PageModes name="Tracks" page="Tracks" logo={<span className="material-icons" style={{ fontSize: "15px" }}>track_changes</span>} />
+            <PageModes name="Albums" page="Tracks" logo={<span className="material-icons" style={{ fontSize: "15px" }}>album</span>} />
+        </>
+    )
+}
+
+function Music() {
+    return (
+        <Template PageMode={<Pages />} width="170px" page="Music" >
+            <div style={{
+                width: "100%",
+                height: "200px",
+                overflow: "hidden"
+            }}>
+                <Overlay />
+            </div>
+            <AllTracks tracks={music} />
+        </Template>
+    )
+}
+
+export default Music
