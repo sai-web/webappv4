fc6d26473c027a4b0765762594ecc1cebd578a6c
saisumith770 <saisumith812@gmail.com>
2020-11-21 19:42:15 +0400

    created the components for music section

---
+++ components/home/music/albumnCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+interface Props {
+    albumn: {
+        thumbnail: string;
+        link: string;
+        title: string;
+        creator: string;
+    }
+}
+
+export const Albumn: React.FC<Props> = ({ albumn }) => {
+    return (
+        <motion.div
+            className="music-albumns"
+            whileTap={{ scale: 0.9 }}
+            style={{
+                width: "220px",
+                height: "300px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "5px",
+                margin: "5px 10px",
+                cursor: "pointer"
+            }}
+        >
+            <img
+                src={albumn.thumbnail}
+                alt="spotify-albumn"
+                style={{
+                    width: "180px",
+                    boxShadow: "0 8px 24px rgba(0,0,0,.5)",
+                    borderRadius: "5px"
+                }}
+            />
+            <h4 style={{ fontFamily: "sans-serif", fontSize: "15px", color: "white" }}>{albumn.title}</h4>
+        </motion.div>
+    )
+}
---
+++ components/home/music/albumnRender.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
+import { albums } from '../../../components/home/fakeData/music'
+import Arrow from '../../../utils/home/arrows'
+
+import { Albumn } from './albumnCard'
+
+const albumnSpliceSize = 4
+type albumnSplice = {
+    thumbnail: string;
+    link: string;
+    title: string;
+    creator: string;
+}[][]
+const albumnSpliced: albumnSplice = albums.reduce((acc: albumnSplice, curr, i) => {
+    if (!(i % albumnSpliceSize)) {
+        acc.push(albums.slice(i, i + albumnSpliceSize))
+    }
+    return acc
+}, [])
+
+export const Albumns = () => {
+    const [pos, setPosition] = useState<number>(0)
+    return (
+        <>
+            <div style={{
+                width: "960px",
+                height: "340px",
+                display: "flex",
+                flexWrap: "wrap",
+                overflow: "hidden",
+                position: "relative",
+                left: "50%",
+                transform: "translate(-50%,0)"
+            }}>
+                <div style={{
+                    width: "2000px",
+                    display: "flex"
+                }}>
+                    {
+                        albumnSpliced.map((albumnGroup, index) => {
+                            return (
+                                <motion.div
+                                    key={index}
+                                    initial={{ x: -300 }}
+                                    animate={{
+                                        x: pos
+                                    }}
+                                    style={{ display: "flex" }}
+                                >
+                                    {albumnGroup.map((album, index) => {
+                                        return (
+                                            <Albumn key={index} albumn={album} />
+                                        )
+                                    })}
+                                </motion.div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <Arrow
+                name="keyboard_arrow_left"
+                positioning={{ left: "30px", top: "240px" }}
+                callback={() => setPosition(state => {
+                    if (state !== 0) return (state + 960)
+                    return 0
+                })}
+            />
+            <Arrow
+                name="keyboard_arrow_right"
+                positioning={{ right: "30px", top: "240px" }}
+                callback={() => setPosition(state => {
+                    if (state !== -960) return state - 960
+                    return -960
+                })}
+            />
+        </>
+    )
+}
