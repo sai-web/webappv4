60753fa3c2a767419230dbd7e3b5718d1f4abacd
saisumith770 <saisumith812@gmail.com>
2020-12-08 19:14:43 +0400

    changed the component arrangements

--- pages/app/subscribed/viewers.tsx
+++ pages/app/subscribed/viewers.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 import { PageModes } from '../../../components/nav/top/components/pageMode'
 import Template from '../../../components/app/template'
 
-import { Analytics } from '../../../components/progress-bar/'
-import { ViewerShipType } from '../../../components/subscription-manager/viewer/categorizing'
-import { Extension } from '../../../components/subscription-manager/viewer/extensions'
+import { Menu } from '../../../components/subscription-manager/viewer/leftMenu'
 
 const Pages: React.FC = () => {
     return (
@@ -20,76 +18,11 @@ function ViewerManager() {
     return (
         <Template PageMode={<Pages />} width="180px" page="Viewer Manager" >
             <div style={{
-                width: "300px",
+                width: "100%",
                 height: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "space-evenly"
+                display: "flex"
             }}>
-                <div style={{
-                    width: "calc(100% - 50px)",
-                    height: "50px",
-                    display: "flex",
-                    justifyContent: "space-evenly",
-                    alignItems: "center"
-                }}>
-                    <ViewerShipType value={24} type="subscribers" />
-                    <ViewerShipType value={10} type="patrons" />
-                    <ViewerShipType value={4} type="streaks" />
-                </div>
-                <div style={{
-                    width: "calc(100% - 50px)",
-                    height: "200px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "space-evenly"
-                }}>
-                    <Extension
-                        icon={
-                            <i className="fa fa-group" style={{
-                                fontSize: "15px",
-                                color: "white"
-                            }}></i>
-                        }
-                        title="Community"
-                        subTitle="reach out to your community"
-                        color="#1ED761"
-                    />
-                    <Extension
-                        icon={
-                            <i className="fa fa-star-o" style={{
-                                fontSize: "15px",
-                                color: "white"
-                            }}></i>
-                        }
-                        title="Merch"
-                        subTitle="create your store to sell merch"
-                        color="#FF9900"
-                    />
-                    <Extension
-                        icon={
-                            <i className="fa fa-send" style={{
-                                fontSize: "15px",
-                                color: "white"
-                            }}></i>
-                        }
-                        title="Feedback"
-                        subTitle="look into the viewers feedback"
-                        color="#4D6FFF"
-                    />
-                </div>
-                <div style={{
-                    width: "calc(100% - 50px)",
-                    height: "200px",
-                    borderRadius: "5px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                    <Analytics percentage={90} />
-                </div>
+                <Menu />
             </div>
         </Template>
     )
