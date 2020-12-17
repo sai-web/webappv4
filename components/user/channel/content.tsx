2cc0ad8d6c1dc6dd726a578e1d5dfa69824a0985
saisumith770 <saisumith812@gmail.com>
2020-12-17 05:56:53 +0400

    i'm lazy

--- components/user/channel/content.tsx
+++ components/user/channel/content.tsx
@@ -1,30 +1,7 @@
 import React from 'react'
 import { mainContentData } from '../../home/fakeData/home'
 
-const ContentFilterWord: React.FC<{ name: string }> = ({ name }) => {
-    return (
-        <div
-            style={{
-                height: "26px",
-                backgroundColor: "#242429",
-                display: "flex",
-                alignItems: "center",
-                marginLeft: "10px",
-                padding: "0 5px",
-                borderRadius: "13px",
-                cursor: "pointer"
-            }}
-        >
-            <h4 style={{
-                lineHeight: "0",
-                fontFamily: "sans-serif",
-                color: "silver",
-                fontWeight: "lighter",
-                fontSize: "13px"
-            }}>{name}</h4>
-        </div>
-    )
-}
+import { ContentFilter } from './filter'
 
 const ContentThumbnail: React.FC<{ thumbnail: string }> = ({ thumbnail }) => {
     return (
@@ -98,43 +75,7 @@ export const Content: React.FC<{ scrolled: boolean }> = ({ scrolled }) => {
             flexDirection: "column",
             alignItems: "center"
         }}>
-            <div style={{
-                width: "calc(100% - 40px)",
-                height: "40px",
-                backgroundColor: "#18181B",
-                borderRadius: "5px",
-                display: "flex",
-                alignItems: "center"
-            }}>
-                <div style={{
-                    width: "calc(100% - 260px)",
-                    overflowX: "scroll",
-                    display: "flex",
-                    alignItems: "center"
-                }} className="main-content-div">
-                    <ContentFilterWord name="twitch" />
-                    <ContentFilterWord name="youtube" />
-                    <ContentFilterWord name="music" />
-                    <ContentFilterWord name="vlogs" />
-                    <ContentFilterWord name="commentary" />
-                </div>
-                <input
-                    type="text"
-                    placeholder="filter content"
-                    style={{
-                        width: "250px",
-                        height: "20px",
-                        marginRight: "10px",
-                        backgroundColor: "#242429",
-                        borderWidth: "0",
-                        borderRadius: "5px",
-                        fontFamily: "sans-serif",
-                        fontSize: "12px",
-                        color: "grey",
-                        paddingLeft: "10px"
-                    }}
-                />
-            </div>
+            <ContentFilter />
             <ChannelContent />
         </div>
     )
--- pages/app/channel/content.tsx
+++ pages/app/channel/content.tsx
@@ -16,7 +16,30 @@ const Pages: React.FC = () => {
 function UserChannel() {
     return (
         <Template PageMode={<Pages />} width="320px" page="Channel" >
-            <h4 style={{ fontFamily: "Poppins", color: "white", fontSize: "40px", lineHeight: "0", marginLeft: "50px", position: "absolute" }}>Trending</h4>
+            <div style={{
+                marginTop: "50px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <h4 style={{
+                    fontFamily: "sans-serif",
+                    color: "silver",
+                    fontSize: "20px",
+                    lineHeight: "0"
+                }}>
+                    Realtime API will come out soon.
+                </h4>
+                <h4 style={{
+                    fontFamily: "sans-serif",
+                    color: "grey",
+                    fontSize: "15px",
+                    lineHeight: "0"
+                }}>
+                    This feature will be available then.
+                </h4>
+            </div>
         </Template>
     )
 }
